989e40783c5a9cb358fef69cbabc53710bdc3748
czDK <168678830@qq.com>
2022-04-23 13:11:46 +0800

    test add main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+void connect() {
+    std::cout << "connect!" << std::endl;
+}
+
+void parse() {
+    std::cout << "parse!" << std::endl;
+}
+
+
+void optimize() {
+    std::cout << "optimize!" << std::endl;
+}
+
+void curd() {
+    std::cout << "curd!" << std::endl;
+}
+
+void result() {
+    std::cout << "result!" << std::endl;
+}
+
+int main() {
+    connect();
+    parse();
+    optimize();
+    curd();
+    result();
+    return 0;
+}
